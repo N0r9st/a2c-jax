4d0191910d9fc563c33b18805fb9d9cea7f83167
Andrey Manoshin <amanoshin@zomb-research-cv-1.zombie.yandex.net>
2022-03-23 19:38:56 +0300

    init vine alg

--- args.py
+++ args.py
@@ -1,3 +1,13 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--wandb-project', type=str, default=None)
+    parser.add_argument('--environment', type=str, default='HalfCheetah-v3')
+    args = parser.parse_args()
+    return args
+
+
 args = dict(
         seed=777,
         gamma=.99,
@@ -20,8 +30,19 @@ args = dict(
         norm_r=True,
         norm_obs=True,
         normalize_advantages=False,
-        prefix='STANDART_', # prefix for wandb name
-        device='1',
+        prefix='', # prefix for wandb name
+        device='0',
         allocate_memory='.15', 
         wandb_proj_name='test_jax_a2c'
     )
+
+cmd_args = parse_args()
+
+def update(args, cmd_args):
+    args['wandb_proj_name'] = cmd_args.wandb_project
+    if cmd_args.wandb_project:
+        
+    args['env_name'] = cmd_args.environment
+    return args
+
+args = update(args, cmd_args)
