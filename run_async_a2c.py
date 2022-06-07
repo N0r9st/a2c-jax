d53d66e1fa84a2cc7867d2433eb5e79ff1d30d17
Andrey Manoshin <amanoshin@zomb-research-cv-1.zombie.yandex.net>
2022-06-07 19:39:05 +0300

    fixed logging

--- run_async_a2c.py
+++ run_async_a2c.py
@@ -387,7 +387,7 @@ def main(args: dict):
                 'time/timestep': timestep, 
                 'time/updates': current_update, 
                 'time/time': epoch_time,
-                'time/num_interactions': interactions_per_epoch,
+                'time/num_interactions': current_update * interactions_per_epoch,
                 'evaluation/score': eval_return,
                 'evaluation/train_score': envs.get_last_return().mean()}, 
                 commit=False, step=current_update)
